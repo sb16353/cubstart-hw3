a9edc6c082407acbbe8e8a190a2a505a3266cdb3
Shreeyans Bhavaraju <shreeyansbhavaraju@wifi-10-42-228-48.wifi.berkeley.edu>
2025-10-03 10:20:08 -0700

    function logic to keeping track of todo list

--- checklist/ViewModel.swift
+++ checklist/ViewModel.swift
@@ -7,20 +7,28 @@
 
 import Foundation
 
+var inProgressTasks: [String] = []
+var completeTasks: [String] = []
+
 class ViewModel: ObservableObject {
-    // TODO: Add class attributes
     
-    func add() -> Void {
-        // TODO: Implement
+    func add(task: String) -> Void {
+        inProgressTasks.append(task)
         return
     }
     
     func swapToInProgress(task: String) -> Void {
-        // TODO: Implement
+        if let index = completeTasks.firstIndex(of: task) {
+                    completeTasks.remove(at: index)
+                    inProgressTasks.append(task)
+                }
     }
     
     func swapToComplete(task: String) -> Void {
-        // TODO: Implement
+        if let index = inProgressTasks.firstIndex(of: task) {
+                   inProgressTasks.remove(at: index)
+                   completeTasks.append(task)
+               }
         return
     }
 }
