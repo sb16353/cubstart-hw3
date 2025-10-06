57496c209f9f0990453167866bdc22b5ad8ce7aa
Shreeyans Bhavaraju <shreeyansbhavaraju@wifi-10-42-231-90.wifi.berkeley.edu>
2025-10-06 11:34:14 -0700

    working process of adding to the in progress checklist and removing it

--- checklist/ContentView.swift
+++ checklist/ContentView.swift
@@ -13,7 +13,7 @@ struct HeaderView: View {
     var body: some View {
         VStack(alignment: .leading) {
             HStack{
-                Image("profile")
+                Image(systemName: "person.circle")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 80, height: 80)
@@ -41,21 +41,50 @@ struct HeaderView: View {
         .padding()
     }
 }
+
+struct ChecklistView: View {
+    @Binding var inProgressTasks: [String]
+    @Binding var completedTasks: [String]
+    var body: some View {
+        List {
+            Section(header: Text("In Progress").textCase(nil)) {
+                ForEach(inProgressTasks, id: \.self) { task in
+                    Button(action: {
+                        if let index = inProgressTasks.firstIndex(of: task) {
+                            completeTasks.append(task)
+                            inProgressTasks.remove(at: index)
+                        }
+                    })
+                    {
+                        HStack {
+                            Image(systemName: "circle")
+                            Text(task)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 struct ContentView: View {
     @State var description = ""
     @State var inProgressTasks: [String] = []
     @State var completedTasks: [String] = []
     var body: some View {
-            VStack {
-                HeaderView(description: $description)
+        VStack {
+            HeaderView(description: $description, addTask: {
                 inProgressTasks.append(description)
                 description = ""
-                ChecklistView(inProgressTasks: $inProgressTasks, completeTasks: $completedTasks)
+            })
+            ChecklistView(inProgressTasks: $inProgressTasks, completedTasks: $completedTasks)
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
